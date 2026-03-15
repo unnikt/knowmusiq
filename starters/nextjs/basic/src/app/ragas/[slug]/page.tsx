d3f218dfc10fe6214fbaab64ab750f77e19503a4
Unni Thottathil <unnikt@gmail.com>
2026-03-15 19:21:50 +1100

    Bug fix ragas\slug\page.tsx

--- starters/nextjs/basic/src/app/ragas/[slug]/page.tsx
+++ starters/nextjs/basic/src/app/ragas/[slug]/page.tsx
@@ -1,15 +1,12 @@
-import { adminDb } from "@/server/firebase-admin";
-
+import { db } from "@/lib/firebase";
+import { collection, limit, query, where, getDocs } from "firebase/firestore";
 export default async function RagaPage({ params }: { params: Promise<{ slug: string }> }) {
 
     const { slug } = await params;
 
+    const q = query(collection(db, "ragas"), where("slug", "==", slug), limit(1));
 
-    const snap = await adminDb.collection("ragas")
-        .where("slug", "==", slug)
-        .limit(1)
-        .get();
-
+    const snap = await getDocs(q);
 
     if (snap.empty) {
         return <div>Raga <strong>{slug}</strong> not found...</div>;
