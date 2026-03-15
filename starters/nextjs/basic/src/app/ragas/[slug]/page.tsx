f3574c8b8b864d88ced41666e051b0e9dd528185
Unni Thottathil <unnikt@gmail.com>
2026-03-15 12:17:30 +1100

    Ragas slug added using firebase admin

---
+++ starters/nextjs/basic/src/app/ragas/[slug]/page.tsx
@@ -0,0 +1,30 @@
+import { db } from "@/lib/firebase-admin";
+// import { doc, getDoc } from "firebase/firestore";
+
+export default async function RagaPage({ params }: { params: Promise<{ slug: string }> }) {
+
+    const { slug } = await params;
+
+    // const ref = doc(db, "ragas", slug);
+    // const snap = await getDoc(ref);
+    const snap = await db.collection("ragas")
+        .where("slug", "==", slug)
+        .limit(1)
+        .get();
+
+    if (snap.empty) {
+        return <div>Raga not found</div>;
+    }
+
+    const raga = snap.docs[0].data();
+
+    return (
+        <div>
+            <h1>{raga.Name}</h1>
+            <p>{raga.Type} Raga</p>
+            <p>Parent: {raga.Parent}</p>
+            <p>Arohana: {raga.Arohana}</p>
+            <p>Arohana: {raga.Avarohana}</p>
+        </div>
+    );
+}
