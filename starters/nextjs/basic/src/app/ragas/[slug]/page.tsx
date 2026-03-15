504069a454431683e3ce3c74b0d12c902c70d4c0
Unni Thottathil <unnikt@gmail.com>
2026-03-15 19:37:22 +1100

    firebase-admin moved to lib

--- starters/nextjs/basic/src/app/ragas/[slug]/page.tsx
+++ starters/nextjs/basic/src/app/ragas/[slug]/page.tsx
@@ -1,12 +1,12 @@
-import { db } from "@/lib/firebase";
-import { collection, limit, query, where, getDocs } from "firebase/firestore";
+import { adminDb } from "@/lib/firebase-admin";
 export default async function RagaPage({ params }: { params: Promise<{ slug: string }> }) {
 
     const { slug } = await params;
 
-    const q = query(collection(db, "ragas"), where("slug", "==", slug), limit(1));
-
-    const snap = await getDocs(q);
+    const snap = await adminDb.collection("ragas")
+        .where("slug", "==", slug)
+        .limit(1)
+        .get();
 
     if (snap.empty) {
         return <div>Raga <strong>{slug}</strong> not found...</div>;
