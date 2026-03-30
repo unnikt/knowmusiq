91c7fa0b98b9030bd01c991643316c8c2036d1aa
Unni Thottathil <unnikt@gmail.com>
2026-03-30 21:46:43 +1100

    Bugfix 2146

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 // app/page.tsx
 
-import MinimiseButton from "@/components/MinimiseButton";
+import { ArrowsPointingOutIcon } from "@heroicons/react/20/solid";
+
 
 export default function Home() {
   const message = process.env["MESSAGE"] || "Scroll ends here...";
+  // const [minHeader, setMinimiseHeader] = useState(false);
 
   return (
     <main>
       {/* <p className="p-4 text-gray-400">{message}</p> */}
-      <section className="flex  justify-center">
-        <i className="text-slate-400 py-4">Click <MinimiseButton /> to toggle header</i>
+      <section className="flex justify-center items-center gap-2 mt-8 p-4 text-gray-400">
+        <span>Click </span>
+        <span><ArrowsPointingOutIcon className="h-5 w-5" /></span>
+        <span> to expand the header</span>
+        <img src="" alt="" />
       </section>
     </main>
   );
