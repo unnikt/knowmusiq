8ff661c072252040190d9169462bba2ec871a994
Unni Thottathil <unnikt@gmail.com>
2026-04-27 20:52:10 +1000

    Update metadata structure in RootLayout for improved SEO and Open Graph support

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,9 +4,31 @@ import "./globals.css";
 import Header from "./Header";
 
 
-export const metadata: Metadata = {
-  title: "knowmusiq",
-  description: "Search and learn about carnatic ragas",
+export const metadata = {
+  title: {
+    default: "musiq-me.com",
+    template: "%s | musiq-me.com",
+  },
+  description: "Learn about Carnatic and Hindustani ragas by exploring their use in movie songs!!",
+  openGraph: {
+    type: "website",
+    siteName: "musiq-me.com",
+    url: "https://musiq-me.com/",
+    description: "Learn about Carnatic and Hindustani ragas by exploring their use in movie songs!!",
+    images: [
+      {
+        url: "https://musiq-me.com/og-default.png",
+        width: 1200,
+        height: 630,
+        alt: "musiq-me.com OG image",
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@musiq-me",
+    images: ["https://musiq-me.com/og-default.png"],
+  },
 };
 
 export default function RootLayout({
